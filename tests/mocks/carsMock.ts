9e728a60bcbe18f77330800d69e6815920a2e782
tormelo <23294699+tormelo@users.noreply.github.com>
2023-02-26 12:13:46 -0300

    test: add tests to the getAll method of CarService

--- tests/mocks/carsMock.ts
+++ tests/mocks/carsMock.ts
@@ -18,3 +18,26 @@ export const validCar: ICar = {
   doorsQty: 2,
   seatsQty: 2,
 };
+
+export const carList: ICar[] = [
+  {
+    id: '634852326b35b59438fbea2f',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    status: false,
+    buyValue: 39,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];
--- tests/unit/Services/car.test.ts
+++ tests/unit/Services/car.test.ts
@@ -3,21 +3,38 @@ import sinon from 'sinon';
 import { Model } from 'mongoose';
 import CarService from '../../../src/Services/CarService';
 import Car from '../../../src/Domains/Car';
-import { validCar, validCarBody } from '../../mocks/carsMock';
+import { carList, validCar, validCarBody } from '../../mocks/carsMock';
 
-describe('Para o método register de CarService', function () {
-  afterEach(function () {
-    sinon.restore();
-  });
+describe('Para o service Car', function () {
+  describe('Para o método register', function () {
+    afterEach(function () {
+      sinon.restore();
+    });
   
-  it('Deve retornar o carro registrado', async function () {
-    sinon.stub(Model, 'create').resolves(validCar);
+    it('Deve retornar o carro registrado', async function () {
+      sinon.stub(Model, 'create').resolves(validCar);
 
-    const carOutput = new Car(validCar);
+      const carOutput = new Car(validCar);
 
-    const service = new CarService();
-    const result = await service.register(validCarBody);
+      const service = new CarService();
+      const result = await service.register(validCarBody);
 
-    expect(result).to.be.deep.equal(carOutput);
-  });  
+      expect(result).to.be.deep.equal(carOutput);
+    });  
+  });
+
+  describe('Para o método getAll', function () {
+    afterEach(function () {
+      sinon.restore();
+    });
+  
+    it('Deve retornar a lista de carros', async function () {
+      sinon.stub(Model, 'find').resolves(carList);
+
+      const service = new CarService();
+      const result = await service.getAll();
+
+      expect(result).to.be.deep.equal(carList);
+    });  
+  });
 });
