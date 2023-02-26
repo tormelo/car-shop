5ffca21aa6635aab7f9dd0d84d4c2c5450a32a14
tormelo <23294699+tormelo@users.noreply.github.com>
2023-02-26 12:12:29 -0300

    feat: add GET /cars endpoint

--- src/Routes/CarRouter.ts
+++ src/Routes/CarRouter.ts
@@ -3,6 +3,11 @@ import CarController from '../Controllers/CarController';
 
 const CarRouter = Router();
 
+CarRouter.get(
+  '/',
+  (req, res, next) => new CarController(req, res, next).getAll(),
+);
+
 CarRouter.post(
   '/',
   (req, res, next) => new CarController(req, res, next).register(),
