f01ec7ce0e077691efead76e7962002bce6db916
tormelo <23294699+tormelo@users.noreply.github.com>
2023-02-26 12:40:44 -0300

    feat: add GET /cars/:id endpoint to CarRouter

--- src/Routes/CarRouter.ts
+++ src/Routes/CarRouter.ts
@@ -4,6 +4,11 @@ import CarController from '../Controllers/CarController';
 const CarRouter = Router();
 
 CarRouter.get(
+  '/:id',
+  (req, res, next) => new CarController(req, res, next).getById(),
+);
+
+CarRouter.get(
   '/',
   (req, res, next) => new CarController(req, res, next).getAll(),
 );
